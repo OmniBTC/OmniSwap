d603d3f0e312bbd054a9f885bdaf610c4c941639
AAweidai <dw1253464613@gmail.com>
2023-03-23 15:53:39 +0800

    Add camelot

--- ethereum/scripts/initialize.py
+++ ethereum/scripts/initialize.py
@@ -489,7 +489,7 @@ def add_dex():
         "DexManagerFacet", SoDiamond[-1].address, DexManagerFacet.abi
     )
     proxy_dex.addDex(
-        "0x9c12939390052919aF3155f41Bf4160Fd3666A6f", {"from": get_account()}
+        "0xc873fEcbd354f5A56E00E710B90EF4201db2448d", {"from": get_account()}
     )
 
 
