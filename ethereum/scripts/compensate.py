515fa9b7fdd9351abe7e058814d2790ba1695ffb
AAweidai <dw1253464613@gmail.com>
2023-04-11 14:03:09 +0800

    Add celer compensate

--- ethereum/scripts/compensate.py
+++ ethereum/scripts/compensate.py
@@ -1,9 +1,33 @@
 # @Time    : 2022/7/15 16:40
 # @Author  : WeiDai
 # @FileName: compensate.py
-from brownie import network, interface, SoDiamond, Contract, chain, StargateFacet, WithdrawFacet
+import functools
+import json
+import os
+
+from brownie import network, interface, SoDiamond, Contract, chain, StargateFacet, WithdrawFacet, config
 
 from scripts.helpful_scripts import get_account, get_stargate_router
+
+
+def is_weth(net, token: str):
+    return config["networks"][net]["token"]["weth"]["address"] == token
+
+
+def zero_address():
+    return "0x0000000000000000000000000000000000000000"
+
+
+@functools.lru_cache
+def chain_info():
+    return {config["networks"][net]["chainid"]: net
+            for net in config["networks"]
+            if "chainid" in config["networks"][net]}
+
+
+def get_net(chainid):
+    info = chain_info()
+    return info[chainid]
 
 
 def compensate_v1():
@@ -44,6 +68,48 @@ def compensate_v2():
     cached_info = stragate.cachedSwapLookup(_srcChainId, _srcAddress, _nonce)
     print("cached_info:", cached_info)
     stragate.clearCachedSwap.call(_srcChainId, _srcAddress, _nonce, {"from": account})
+
+
+def compensate_v3():
+    cmd = "curl --silent https://api.celerscan.com/scan/searchByTxHash?tx={tx}"
+    receivers = {
+        "0xb284673efaf4b855738e835f6ed8f6343095566fe6870671b89b8c668534c1c0": "0x0c8cD6Fa227e8fAF1330B265e496E87846b4f9Bc",
+        "0x70d6119f7c76ce87291a747347cd2f9e65a4fbd74398eff9633b3c51668b319d": "0xb380BcB74261956BA7Fa7b7baE1F004383017e54",
+        "0x848232e697e548a1ff15fdff4a20ccaaf1f6afd73b19d21b17b35c1437258c34": "0x6a8943b4c779CC30199832A5224280bbaAE02914",
+        "0x69a3a3f4b0469593bcecfc5065b8b9c1826388b4d2904f1de6abeee6ee471dc0": "0xfF0b9681ef40Fd0992Ef52fD5bd94E0Fa21c0359",
+        "0xf8dcf2fd9df5a1ef43e378e60d635f0adbe0c85169ac88bfb1c8873e0b971f65": "0xA9cC6e06b05eE600b0b58b839B74B981608e055a",
+        "0x27e462015f0650dfeb92fb1feb15e7a81a828ab8529f01f19302e39e6977b000": "0x0e73b32E493D081Ee6b2C4766d7478F867B7374b",
+        "0x0bfcdd38ff33e184a83fc3a8e429e23b4294c161a0534143f90e90aa69cc9756": "0x69527FEF6AEDb85FDD2851FC1D9F6D10e61864aA",
+    }
+    account = get_account()
+    withdraw_contract = Contract.from_abi("WithdrawFacet", "0x2967E7Bb9DaA5711Ac332cAF874BD47ef99B3820",
+                                          WithdrawFacet.abi)
+    for src_txid in receivers:
+        cur_cmd = cmd.replace("{tx}", src_txid)
+        with os.popen(cur_cmd) as f:
+            result = json.load(f)
+            result = result["txSearchInfo"][0]
+        if len(result["message"]) == 0 or len(result["transfer"]) == 0:
+            continue
+        if "execution_tx" in result["message"][0] and result["message"][0]["msg_status"] != 3:
+            received_amt = result["transfer"][0]["received_amt"]
+            dst_token_addr = result["transfer"][0]["dst_token_addr"]
+            dst_txid = result['message'][0]['execution_tx']
+            dst_chain_id = result["transfer"][0]["dst_chain_id"]
+            src_chain_id = result["base_info"]["src_chain_id"]
+            src_net = get_net(src_chain_id)
+            dst_net = get_net(dst_chain_id)
+            receiver = receivers[src_txid]
+            is_weth_flag = is_weth(dst_net, dst_token_addr)
+            actual_token_addr = zero_address() if is_weth_flag else dst_token_addr
+            print(f"src: {src_net}, {src_txid}, dst: {dst_net}, {dst_txid} "
+                  f"received_amt:{received_amt}, dst_token_addr:{dst_token_addr}, "
+                  f"receiver:{receiver}, is_weth:{is_weth_flag}, actual_token_addr:{actual_token_addr}\n")
+            withdraw_contract.withdraw(actual_token_addr,
+                                       receiver,
+                                       received_amt,
+                                       {"from": account}
+                                       )
 
 
 def withdraw():
