66afabd354b6f5a824ead89cf89fe5ece08de5d2
icodezjb <icodezjb@users.noreply.github.com>
2023-04-14 15:39:49 +0800

    Add compensate_v4 for multichain

--- ethereum/scripts/compensate.py
+++ ethereum/scripts/compensate.py
@@ -16,7 +16,7 @@ from brownie import (
     config,
 )
 
-from scripts.helpful_scripts import get_account, get_stargate_router
+from scripts.helpful_scripts import get_account, get_stargate_router, change_network
 
 
 def is_weth(net, token: str):
@@ -125,6 +125,92 @@ def compensate_v3():
             withdraw_contract.withdraw(
                 actual_token_addr, receiver, received_amt, {"from": account}
             )
+
+
+def compensate_v4():
+    cmd = "curl --silent https://scanapi.multichain.org/v3/tx/{tx}"
+    receivers = {
+        "mainnet": [
+            {
+                "tx": "0x392c0f395b196a9e05b38c3c94e2cb22d86ebb96e778b611fa0da426f577a031",
+                "receiver": "0xaE3e419dA4740bA6AF85fcAE315f696fE6F8841d",
+            },
+            {
+                "tx": "0x1060668e2aaf4204c9f6b76bb6ba008314c2279276c76b54ce14f6e1fa1ac747",
+                "receiver": "0xe52aE1C32AF9E2d1ea1B180998a0cd4adC27F00E",
+            },
+        ],
+        "avax-main": [
+            {
+                "tx": "0xa4ddb4499e191b3028c1bb3a84eb5a39dc7f5f506e3d43bf330063c6720852a1",
+                "receiver": "0x372D0E22Ad7Db405bfe453f5b120ECDeDd65E1A2",
+            }
+        ],
+        "bsc-main": [
+            {
+                "tx": "0xe2db5516c997c997127350ac891ed467c525c348f27c62bd41e4ecac19902815",
+                "receiver": "0xC24b755A3071BA5C5833C17a8B76A00fF212B935",
+            },
+            {
+                "tx": "0xffb3d2ad8b3463d0c0a7681122e63f7ac5b6f13f6171909734a027ea7518b244",
+                "receiver": "0x5F94D02B0B6087D22cDF40339df7796D0f93CC0A",
+            },
+            {
+                "tx": "0xa8f2b5ebcee7777a749091cf6ed34bd9ced7b144dc78de9a4ccfaba6d688447b",
+                "receiver": "0xDFAB516FE8B7c06e06E725F9d0C1735Ad1B2558D",
+            },
+            {
+                "tx": "0x5ce298e82a2b64a8b2e9492e493264ec42666ee30842a9dab8ab1825518bfb85",
+                "receiver": "0x5f72B0A205d655a2c03bcaF87A45600c386101C7",
+            },
+        ],
+        "optimism-main": [
+            {
+                "tx": "0xd4c668e038de04a772efdb3473fe330d5e43e6e4b72dd5ec68c37d8629d8dc30",
+                "receiver": "0x38E25E37dca1c7b672235c14E592acDED7F0944D",
+            },
+            {
+                "tx": "0xfe529fe4a8b31d1f006ecce50dafdc9ae375108788d787689e01a6041373062e",
+                "receiver": "0xfa6ECa16A900EFDf94027D1e30EA16310a7C37A7",
+            },
+            {
+                "tx": "0x65c933e53c4234b4da7d364bc65e1dc27655b24a1b6fc926d29ec28023f13200",
+                "receiver": "0x1487d6559001964Ff1CF52570176Eecec145f9C4",
+            },
+        ],
+    }
+
+    account = get_account()
+    withdraw_contract = Contract.from_abi(
+        "WithdrawFacet", "0x2967E7Bb9DaA5711Ac332cAF874BD47ef99B3820", WithdrawFacet.abi
+    )
+
+    for (network, txs) in receivers.items():
+        change_network(network)
+
+        for src_tx in txs:
+            curl_cmd = cmd.replace("{tx}", src_tx["tx"])
+            receiver = src_tx["receiver"]
+            with os.popen(curl_cmd) as f:
+                result = json.load(f)
+                swaptx = result["info"]["swaptx"]
+                symbol = result["info"]["swapinfo"]["routerSwapInfo"]["tokenID"]
+
+                tx = chain.get_transaction(swaptx)
+
+                anyToken = tx.events["LogAnySwapInAndExec"]["token"]
+                amount = tx.events["LogAnySwapInAndExec"]["amount"]
+
+                proxy_anyToken = Contract.from_abi(
+                    "IMultiChainUnderlying",
+                    anyToken,
+                    interface.IMultiChainUnderlying.abi,
+                )
+                token = proxy_anyToken.underlying()
+
+                print(network, receiver, token, symbol, amount)
+
+                withdraw_contract.withdraw(token, receiver, amount, {"from": account})
 
 
 def withdraw():
