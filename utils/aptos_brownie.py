ab8bf61ee98434a2cb79bc3067140b03f07fd4d3
AAweidai <dw1253464613@gmail.com>
2023-04-28 17:48:46 +0800

    Fix get resource

--- utils/aptos_brownie.py
+++ utils/aptos_brownie.py
@@ -758,25 +758,33 @@ class AptosPackage:
 
     def account_resources(self,
                           account_addr: Union[str, AccountAddress],
-                          limit=None
+                          limit=None,
+                          cursor=None
                           ):
         if isinstance(account_addr, str):
             account_addr = AccountAddress.from_hex(account_addr)
-        if limit is None:
-            response = self.rest_client.client.get(
-                f"{self.rest_client.base_url}/accounts/{account_addr}/resources"
-            )
-            is_limit = False
-        else:
-            response = self.rest_client.client.get(
-                f"{self.rest_client.base_url}/accounts/{account_addr}/resources?limit={int(limit)}"
-            )
-            is_limit = True
+        data = []
+        is_cursor = False
+        if cursor is not None:
+            data.append(f"start={cursor}")
+            is_cursor = True
+
+        if limit is not None:
+            data.append(f"limit={int(limit)}")
+            is_cursor = True
+
+        if len(data):
+            data = "?" + "&".join(data)
+
+        response = self.rest_client.client.get(
+            f"{self.rest_client.base_url}/accounts/{account_addr}/resources{data}"
+        )
+
         if response.status_code == 404:
             return None
         if response.status_code >= 400:
             raise ApiError(f"{response.text} - {account_addr}", response.status_code)
-        if is_limit:
+        if is_cursor:
             return response.json(), response.headers.get('x-aptos-cursor', None)
         else:
             return response.json()
