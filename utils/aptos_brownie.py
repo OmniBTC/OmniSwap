b66a04b53537319826ac60ae1259f4f26fde008f
AAweidai <dw1253464613@gmail.com>
2023-05-04 16:01:07 +0800

    Fix simulate

--- utils/aptos_brownie.py
+++ utils/aptos_brownie.py
@@ -573,11 +573,12 @@ class AptosPackage:
         )
         try:
             result = self.simulate_submit_bcs_transaction(signed_transaction)
-            if not result[0]["success"]:
-                assert False, result
             if return_types is None:
                 return result
-            elif return_types == "gas":
+            if not result[0]["success"]:
+                assert False, result
+
+            if return_types == "gas":
                 if "gas_used" in result[0]:
                     return result[0]["gas_used"]
             else:
