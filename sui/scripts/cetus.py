e3528557e3422c1247823402240dc0893005aa08
hacpy <hacpy.lbx@gmail.com>
2023-05-06 10:54:41 +0800

    Add cetus test script

---
+++ sui/scripts/cetus.py
@@ -0,0 +1,99 @@
+from sui_brownie import SuiPackage
+
+from scripts import sui_project
+from scripts.deploy import clock
+
+
+def global_config():
+    return sui_project.network_config['objects']['GlobalConfig']
+
+
+def btc():
+    return sui_project.network_config['tokens']['Cetus-BTC']['address']
+
+
+def btc_supply():
+    return sui_project.network_config['objects']['BTCSupply']
+
+
+def eth():
+    return sui_project.network_config['tokens']['Cetus-ETH']['address']
+
+
+def eth_supply():
+    return sui_project.network_config['objects']['ETHSupply']
+
+
+def usdc():
+    return sui_project.network_config['tokens']['Cetus-USDC']['address']
+
+
+def usdc_supply():
+    return sui_project.network_config['objects']['USDCSupply']
+
+
+def usdt():
+    return sui_project.network_config['tokens']['Cetus-USDT']['address']
+
+
+def usdt_supply():
+    return sui_project.network_config['objects']['USDTSupply']
+
+
+def usdc_eth_pool():
+    return sui_project.network_config['pools']['Cetus-USDC-ETH']['pool_id']
+
+
+def load_cetus_faucet():
+    return SuiPackage(
+        sui_project.network_config['packages']['CetusFaucet'],
+        package_name="CetusFaucet",
+    )
+
+
+def load_cetus_scripts():
+    return SuiPackage(
+        sui_project.network_config['packages']['CetusScripts'],
+        package_name="CetusScripts",
+    )
+
+
+def claim_from_cetus_faucet(token):
+    faucet = load_cetus_faucet()
+    if "BTC" in token:
+        faucet.btc.faucet(btc_supply())
+    elif "ETH" in token:
+        faucet.eth.faucet(eth_supply())
+    elif "USDC" in token:
+        faucet.usdc.faucet(usdc_supply())
+    elif "USDT" in token:
+        faucet.usdt.faucet(usdt_supply())
+    else:
+        raise Exception("Invalid token")
+
+
+def get_coins(coin_type):
+    result = sui_project.client.suix_getCoins(sui_project.account.account_address, coin_type, None, None)
+    return [c["coinObjectId"] for c in result["data"]]
+
+
+def add_liquidity():
+    cetus_scripts = load_cetus_scripts()
+
+    cetus_scripts.pool_script.open_position_with_liquidity_with_all(
+        global_config(),
+        usdc_eth_pool(),
+        52,
+        4294967244,
+        get_coins(usdc()),
+        get_coins(eth()),
+        int(200 * 1e6),
+        int(0.1 * 1e8),
+        True,
+        clock(),
+        type_arguments=[usdc(), eth()]
+    )
+
+
+if __name__ == "__main__":
+    add_liquidity()
