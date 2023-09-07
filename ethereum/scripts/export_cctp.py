8769f7f32d557f6171eb0ffe8c4401575759adfb
AAweidai <dw1253464613@gmail.com>
2023-09-07 13:17:31 +0800

    Reexport path

--- ethereum/scripts/export_cctp.py
+++ ethereum/scripts/export_cctp.py
@@ -24,7 +24,7 @@ def read_json(file):
 
 
 def export_cctp_info():
-    support_networks = ['arbitrum-main', 'avax-main', "optimism-main"]
+    support_networks = ['arbitrum-main', 'avax-main', "optimism-main", "mainnet"]
 
     cctp_bridge_token_info = {}
     for network in support_networks:
