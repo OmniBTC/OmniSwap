6cf38447799d2b1dbe7ffe550297c0b75799e1e0
icodezjb <icodezjb@users.noreply.github.com>
2023-10-27 16:52:12 +0800

    Add check_request.py

---
+++ solana/scripts/check_request.py
@@ -0,0 +1,30 @@
+import asyncio
+from omniswap.accounts.cross_request import CrossRequest
+from helper import deriveCrossRequestKey
+from config import get_client, get_config
+
+
+async def omniswap_estimate_relayer_fee(
+        network="devnet",
+):
+    client = get_client(network)
+    await client.is_connected()
+
+    config = get_config(network)
+
+    omniswap_program_id = config["program"]["SoDiamond"]
+
+    start = 0
+    while True:
+        req_key = deriveCrossRequestKey(omniswap_program_id, sequence=start)
+
+        req = await CrossRequest.fetch(client, req_key)
+        if req is None:
+            print(f"request={req_key}, None")
+        else:
+            print(f"request={req_key}, Some")
+
+        start += 1
+
+
+asyncio.run(omniswap_estimate_relayer_fee())
