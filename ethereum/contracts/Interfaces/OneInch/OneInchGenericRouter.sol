290597c77806509f67aa1387124ab538932d005d
hacpy <hacpy.lbx@gmail.com>
2023-10-31 15:22:08 +0800

    Add one inch router interfaces

---
+++ ethereum/contracts/Interfaces/OneInch/OneInchGenericRouter.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/// @title Interface for making arbitrary calls during swap
+interface IAggregationExecutor {
+    /// @notice propagates information about original msg.sender and executes arbitrary data
+    function execute(address msgSender) external payable;  // 0x4b64e492
+}
+
+interface OneInchGenericRouter {
+    struct SwapDescription {
+        IERC20 srcToken;
+        IERC20 dstToken;
+        address payable srcReceiver;
+        address payable dstReceiver;
+        uint256 amount;
+        uint256 minReturnAmount;
+        uint256 flags;
+    }
+
+    /// @notice Performs a swap, delegating all calls encoded in `data` to `executor`. See tests for usage examples
+    /// @dev router keeps 1 wei of every token on the contract balance for gas optimisations reasons. This affects first swap of every token by leaving 1 wei on the contract.
+    /// @param executor Aggregation executor that executes calls described in `data`
+    /// @param desc Swap description
+    /// @param permit Should contain valid permit that can be used in `IERC20Permit.permit` calls.
+    /// @param data Encoded calls that `caller` should execute in between of swaps
+    /// @return returnAmount Resulting token amount
+    /// @return spentAmount Source token amount
+    function swap(
+        IAggregationExecutor executor,
+        SwapDescription calldata desc,
+        bytes calldata permit,
+        bytes calldata data
+    )
+    external
+    payable
+    returns (
+        uint256 returnAmount,
+        uint256 spentAmount
+    );
+}
---
+++ ethereum/contracts/Interfaces/OneInch/OneInchUnoswapRouter.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface OneInchUnoswapRouter {
+    /// @notice Same as `unoswapTo` but calls permit first,
+    /// allowing to approve token spending and make a swap in one transaction.
+    /// @param recipient Address that will receive swapped funds
+    /// @param srcToken Source token
+    /// @param amount Amount of source tokens to swap
+    /// @param minReturn Minimal allowed returnAmount to make transaction commit
+    /// @param pools Pools chain used for swaps. Pools src and dst tokens should match to make swap happen
+    /// @param permit Should contain valid permit that can be used in `IERC20Permit.permit` calls.
+    /// See tests for examples
+    function unoswapToWithPermit(
+        address payable recipient,
+        IERC20 srcToken,
+        uint256 amount,
+        uint256 minReturn,
+        uint256[] calldata pools,
+        bytes calldata permit
+    ) external returns (uint256 returnAmount);
+
+    /// @notice Performs swap using Uniswap exchange. Wraps and unwraps ETH if required.
+    /// Sending non-zero `msg.value` for anything but ETH swaps is prohibited
+    /// @param recipient Address that will receive swapped funds
+    /// @param srcToken Source token
+    /// @param amount Amount of source tokens to swap
+    /// @param minReturn Minimal allowed returnAmount to make transaction commit
+    /// @param pools Pools chain used for swaps. Pools src and dst tokens should match to make swap happen
+    function unoswapTo(
+        address payable recipient,
+        IERC20 srcToken,
+        uint256 amount,
+        uint256 minReturn,
+        uint256[] calldata pools
+    ) external payable returns (uint256 returnAmount);
+
+    /// @notice Performs swap using Uniswap exchange. Wraps and unwraps ETH if required.
+    /// Sending non-zero `msg.value` for anything but ETH swaps is prohibited
+    /// @param srcToken Source token
+    /// @param amount Amount of source tokens to swap
+    /// @param minReturn Minimal allowed returnAmount to make transaction commit
+    /// @param pools Pools chain used for swaps. Pools src and dst tokens should match to make swap happen
+    function unoswap(
+        IERC20 srcToken,
+        uint256 amount,
+        uint256 minReturn,
+        uint256[] calldata pools
+    ) external payable returns (uint256 returnAmount);
+}
