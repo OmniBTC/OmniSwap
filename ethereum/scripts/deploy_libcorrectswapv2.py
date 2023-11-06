7fea9a41fc5d8b00c26a31b348bbbe264f6f3ca1
AAweidai <dw1253464613@gmail.com>
2023-11-06 17:35:46 +0800

    Fix script

--- ethereum/scripts/deploy_libcorrectswapv2.py
+++ ethereum/scripts/deploy_libcorrectswapv2.py
@@ -26,56 +26,29 @@ def deploy_correct_swaps(account):
     print("deploy LibCorrectSwapV2...")
     lib = LibCorrectSwapV2.deploy({"from": account})
 
-    print("deploy CorrectUniswapV2Factory...")
-    CorrectUniswapV2Factory.deploy(lib.address, {"from": account})
+    factorys = [
+        CorrectUniswapV2Factory,
+        CorrectUniswapV3Factory,
+        CorrectSyncswapFactory,
+        CorrectMuteswapFactory,
+        CorrectQuickswapV3Factory,
+        CorrectAerodromeFactory,
+        CorrectBalancerV2Factory,
+        CorrectCurveFactory,
+        CorrectWombatFactory,
+        CorrectTraderJoeFactory,
+        CorrectGMXV1Factory,
+        CorrectPearlFiFactory,
+        CorrectIZiSwapFactory,
+        CorrectCamelotFactory,
+        CorrectKyberswapFactory,
+        CorrectOneInchFactory,
+        CorrectOpenOceanFactory,
+    ]
 
-    print("deploy CorrectUniswapV3Factory...")
-    CorrectUniswapV3Factory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectSyncswapFactory...")
-    CorrectSyncswapFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectMuteswapFactory...")
-    CorrectMuteswapFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectQuickswapV3Factory...")
-    CorrectQuickswapV3Factory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectAerodromeFactory...")
-    CorrectAerodromeFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectBalancerV2Factory...")
-    CorrectBalancerV2Factory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectCurveFactory...")
-    CorrectCurveFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectWombatFactory...")
-    CorrectWombatFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectTraderJoeFactory...")
-    CorrectTraderJoeFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectGMXV1Factory...")
-    CorrectGMXV1Factory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectPearlFiFactory...")
-    CorrectPearlFiFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectIZiSwapFactory...")
-    CorrectIZiSwapFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectCamelotFactory...")
-    CorrectCamelotFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectKyberswapFactory...")
-    CorrectKyberswapFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectOneInchFactory...")
-    CorrectOneInchFactory.deploy(lib.address, {"from": account})
-
-    print("deploy CorrectOpenOceanFactory...")
-    CorrectOpenOceanFactory.deploy(lib.address, {"from": account})
+    for k, factory in enumerate(factorys):
+        print(f"deploy {k}/{len(factorys)} {factory._name}.sol...")
+        factory.deploy(lib.address, {"from": account})
 
 
 def main():
