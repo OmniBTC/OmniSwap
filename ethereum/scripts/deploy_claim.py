2e64532877a79daf8f0e14647c0432e444f80764
AAweidai <dw1253464613@gmail.com>
2023-11-15 11:20:10 +0800

    Fix script

--- ethereum/scripts/deploy_claim.py
+++ ethereum/scripts/deploy_claim.py
@@ -78,35 +78,27 @@ def generate_proof():
 
 
 def claim_test():
-    account: Account = get_account()
+    account: Account = get_account("deploy_key")
 
-    index = 808
-    amount = 2467489637769138280061
+    index = 152
+    amount = 2982610
     proof = [
-        "0x71087c4d990909323ac5fe7efadb7e2b630d482d0192be03c96da964b3163a6f",
-        "0x74ba0035eb40d276197a366e6793c94500df39ceabbeea8f9de1e46186ee0440",
-        "0x7c5976b7c29df481480e5d6215c30f13abefa31e7eac1c3164fd23b48128ae3f",
-        "0x1241e63c00d69ac02e8fdcbc5925d4a49dafe0e23f1fbfa5cd33761533c7d14b",
-        "0x8e6ff94322bb620a53a2a58aeadfb28074f7706345e282a85db3e6db8cedf46a",
-        "0x70ba4c5be4736525cbf00b834cae36ce46d17499ca8b16297336b56546895c5c",
-        "0xf75db365841adacb53eb699d0dc0773734eee52fb9730e5ff6f2dedb7f0bbd74",
-        "0x449c88bf14260a967529033b3acd71b004f2cc8644de24c1cc93e215a7d9e132",
-        "0xaead6e5191d458c7e115584edcfb7262d11a13bfd4375e1a7ea6c9286c4ab566",
-        "0x400d126f35c6fbbb437c42decb116541945114b915ea41654ef5eda52f00a510",
-        "0x4eaf2844f368dbe2c98f9513f488c92c26a5a0c9dbd58586c5763e95113399f4",
-        "0xeecb42086fac38b632e53fb848900544fcd40d14a07aaba61fcb9f83d8806cad",
-        "0xf411c5ba74f39059b3823080dd25e3bf2a09fc15601395577509674d82027e7a",
-        "0x6b942e058a31016399412c20008b5bf23f96e04778cd279dc8767910cdde6b9c",
-        "0x636d83c93bf376bfd7a51df6607cedafea762c8d430db4fc2187265e2c389d07"
+        "0x1e112fd4b580356956aa2c3f509b30bc566389f2a0cddf61d261c7a03e92eeb8",
+        "0x20164d627e3006f7df7d47487500cd0ce526bb53bc715cdba27dedbb7a0b4771",
+        "0x83cbae1b310a351d33eccbb3143a5772adab26b904096e807bea35e658da2598",
+        "0xa3ef647496e97aa5d2e3a06fe838f7cd9bb571625622f99252fb28f82d87e409",
+        "0xc3e0c3d1cd6f4a979894124df7f36a928bd910949e8eb1df2d3d65e58a1a0d25",
+        "0x707578c46bc73214a0b075103e094217c7af9fd26b6a8d160447587b0bf8c5b5",
+        "0x9673984e49a3dd103adb9af777d5752773275eba27c847f94f2f377a02208c82",
+        "0x7c93a63fa304310cebf744fee5d4861678f48a3b29bbb10208a5437f81bcfc97",
+        "0x98d72f54292e0f218430beadf30b31997b1bd83fa2df4fc7516dd9ee0ecf08e0",
+        "0x953428c56793fba0d98258836a4f72c88d45eb8d4e7d5d13ddb799e2ad38b04d"
     ]
-    # MockToken[-1].transfer(Claim[-1].address, amount, {"from": account})
-    new_account = accounts.add("0xdd04778b596300c3d5b3fce9e00bfbfdc21af16a69d515ccaeb000307f41e2fe")
-    # account.transfer(new_account, int(0.0001 * 1e18))
-    Claim[-1].claim(index, amount, proof, {"from": new_account})
+    Claim[-1].claim(index, amount, proof, {"from": account})
 
 
 def deploy_op():
-    account = get_account()
+    account = get_account("deploy_key")
     if "test" in network.show_active():
         MockToken.deploy("OP", "OP", {"from": account})
         token_address = MockToken[-1].address
@@ -118,7 +110,7 @@ def deploy_op():
 
 
 def deploy_arb():
-    account = get_account()
+    account = get_account("deploy_key")
     if "test" in network.show_active():
         MockToken.deploy("OP", "OP", {"from": account})
         token_address = MockToken[-1].address
@@ -130,11 +122,23 @@ def deploy_arb():
     Claim[-1].pause({"from": account})
 
 
+def deploy_poly():
+    account = get_account("deploy_key")
+    if "test" in network.show_active():
+        MockToken.deploy("OP", "OP", {"from": account})
+        token_address = MockToken[-1].address
+    else:
+        token_address = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
+    start = int(time.time() + 10)
+    merkle_root = "0x0af56c4a460194df83b31f068a4c5bf9a915e97532de75437f651c488f3fd7d2"
+    Claim.deploy(start, token_address, merkle_root, {"from": account})
+
+
 def pause():
     account = get_account("deploy_key")
     c = Contract.from_abi("Claim", "0xE56a4CcfEaDb140333b91f561C6B0cF308450ac5", Claim.abi)
     # c.pause({"from": account})
-    
+
     token_address = "0x4200000000000000000000000000000000000042"
     token = Contract.from_abi("Token", token_address, MockToken.abi)
     balance = token.balanceOf(c.address)
