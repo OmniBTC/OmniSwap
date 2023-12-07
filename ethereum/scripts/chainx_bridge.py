86a3035c0fdff30a0da8962f21819a8644cf90d4
AAweidai <dw1253464613@gmail.com>
2023-12-08 07:34:40 +0800

    Fix bridge

--- ethereum/scripts/chainx_bridge.py
+++ ethereum/scripts/chainx_bridge.py
@@ -20,17 +20,23 @@ def main():
     chainx_bridge = load_chainx_bridge()
     acc = get_account()
     amount = int(1 / 1e8 * 1e18)
-
-    start = acc.nonce
-    end = 1000
-    for _ in range(start, end):
-        swap_id = f"0x{secrets.token_bytes(32).hex()}"
-        swap_id = swap_id[:-4] + "cccc"
-        chainx_bridge.swap_out(
-            swap_id,
-            10,
-            str(acc.address)[2:],
-            amount,
-            100,
-            {"from": acc, "value": amount}
-        )
+    print("nonce", acc.nonce)
+    while True:
+        start = acc.nonce
+        end = 1000
+        try:
+            for _ in range(start, end):
+                swap_id = f"0x{secrets.token_bytes(32).hex()}"
+                swap_id = swap_id[:-4] + "cccc"
+                chainx_bridge.swap_out(
+                    swap_id,
+                    10,
+                    str(acc.address)[2:],
+                    amount,
+                    100,
+                    {"from": acc, "value": amount}
+                )
+        except Exception as e:
+            print("Error:", e)
+        if acc.nonce >= end:
+            break
