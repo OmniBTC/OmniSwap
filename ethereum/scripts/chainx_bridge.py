14c6ee91aacc2a490151f15bbe1c57d84b058f19
AAweidai <dw1253464613@gmail.com>
2023-12-07 17:25:19 +0800

    Add chainx bridge

---
+++ ethereum/scripts/chainx_bridge.py
@@ -0,0 +1,35 @@
+import secrets
+
+from brownie import (
+    ChainXBridge,
+    Contract,
+    config, network
+)
+
+from scripts.helpful_scripts import get_account
+
+
+def load_chainx_bridge():
+    return Contract.from_abi(
+        "ChainXBridge",
+        config["networks"][network.show_active()]["chainx_bridge"]
+        , ChainXBridge.abi)
+
+
+def main():
+    chainx_bridge = load_chainx_bridge()
+    acc = get_account()
+    amount = int(1 / 1e8 * 1e18)
+
+    count = 999
+    for _ in range(count):
+        swap_id = f"0x{secrets.token_bytes(32).hex()}"
+        swap_id = swap_id[:-4] + "cccc"
+        chainx_bridge.swap_out(
+            swap_id,
+            10,
+            str(acc.address)[2:],
+            amount,
+            100,
+            {"from": acc, "value": amount}
+        )
