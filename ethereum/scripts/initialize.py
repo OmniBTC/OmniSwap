7ebd3003653c84a17e0d266b1a67134a4dce00fb
AAweidai <dw1253464613@gmail.com>
2024-04-18 09:08:40 +0800

    Redeploy connext to update correct swap

--- ethereum/scripts/initialize.py
+++ ethereum/scripts/initialize.py
@@ -669,26 +669,26 @@ def redeploy_connext():
 
     # 1. deploy connext's lib so fee
 
-    so_fee = 1e-3
-    ray = 1e27
-    basic_beneficiary = config["networks"][network.show_active()]["bridges"]["connext"][
-        "basic_beneficiary"
-    ]
-    basic_fee = config["networks"][network.show_active()]["bridges"]["connext"][
-        "basic_fee"
-    ]
-    print(
-        f"Net:{network.show_active()} basic_beneficiary:{basic_beneficiary} basic_fee:{basic_fee}"
-    )
-    LibSoFeeConnextV2.deploy(
-        int(so_fee * ray), basic_fee, basic_beneficiary, {"from": account}
-    )
-
-    # 2. add connext's lib so fee to diamond
-    proxy_dex = Contract.from_abi(
-        "DexManagerFacet", SoDiamond[-1].address, DexManagerFacet.abi
-    )
-    proxy_dex.addFee(get_connext(), LibSoFeeConnextV2[-1].address, {"from": account})
+    # so_fee = 1e-3
+    # ray = 1e27
+    # basic_beneficiary = config["networks"][network.show_active()]["bridges"]["connext"][
+    #     "basic_beneficiary"
+    # ]
+    # basic_fee = config["networks"][network.show_active()]["bridges"]["connext"][
+    #     "basic_fee"
+    # ]
+    # print(
+    #     f"Net:{network.show_active()} basic_beneficiary:{basic_beneficiary} basic_fee:{basic_fee}"
+    # )
+    # LibSoFeeConnextV2.deploy(
+    #     int(so_fee * ray), basic_fee, basic_beneficiary, {"from": account}
+    # )
+    #
+    # # 2. add connext's lib so fee to diamond
+    # proxy_dex = Contract.from_abi(
+    #     "DexManagerFacet", SoDiamond[-1].address, DexManagerFacet.abi
+    # )
+    # proxy_dex.addFee(get_connext(), LibSoFeeConnextV2[-1].address, {"from": account})
 
     try:
         remove_facet(ConnextFacet)
@@ -793,6 +793,7 @@ def redeploy_bool():
     LibSoFeeBoolV2.deploy(
         int(so_fee * ray), basic_fee, basic_beneficiary, {"from": account}
     )
+    print(
         f"LibSoFeeBoolV2 deploy Net:{network.show_active()} basic_beneficiary:{basic_beneficiary} basic_fee:{basic_fee}"
     )
     LibSoFeeBoolV2.deploy(
