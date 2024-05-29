ad164c6c4f251a2328d5f45a0e67a0c991324e58
AAweidai <dw1253464613@gmail.com>
2024-05-29 12:42:29 +0800

    Fix scripts

--- ethereum/scripts/deploy_genesis_box.py
+++ ethereum/scripts/deploy_genesis_box.py
@@ -10,5 +10,7 @@ def deploy_genesis_box_staking(nft_contract_address):
 
 
 def main():
-    nft_contract_address = "0x09Ff8E49D0EA411A3422ed95E8f5497D4241F532"
+    # testnet: 0x0AC2782a9f6Aef6073792c56ea8292c45Fc7c12B
+    # mainnet: 0x09Ff8E49D0EA411A3422ed95E8f5497D4241F532
+    nft_contract_address = "0x0AC2782a9f6Aef6073792c56ea8292c45Fc7c12B"
     deploy_genesis_box_staking(nft_contract_address)
